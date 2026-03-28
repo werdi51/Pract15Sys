6e5ac1c002ad8c1fa44d4bd93544b4c3fcb9d6b4
werdi51 <spotifig2@gmail.com>
2026-03-28 09:56:39 +0500

    Start

---
+++ Pract15Sys/Pract15Sys.cpp
@@ -0,0 +1,24 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iphlpapi.h>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+struct addrinfo
+{
+	*result = NULL
+};
+
+int main()
+{
+
+	
+
+
+
+}
+
